a7903c0324fcef7ad14ff4dc67193340afd5b181
Lex Zavala <lex.zavala7@gmail.com>
2021-07-07 09:12:39 -0500

    added card container code to page

--- js/script.js
+++ js/script.js
@@ -16,11 +16,33 @@ var teamsParameters = {
 const getTeams = () => {
     $.ajax(teamsParameters).done((teams) => {
         console.log(teams);
-        setTimeout(function (){
+        setTimeout(function () {
             let htmlStr = '';
-            for (let team of teams){
+            for (let team of teams) {
                 htmlStr += `<div><h3>${team.City}</h3></div>
             <div><img class="teamLogo" src=${team.WikipediaLogoUrl}></div>
+            //////
+            <div class="mainContainer d-flex justify-content-center align-items-center">
+                <div class="cardContainer">
+                    <div class="sneaker d-flex align-items-center justify-content-center">
+                        <div class="circle"></div>
+                        <img src="img/nike-blue-shoe.png">
+                    </div>
+            <div class="info">
+                <h1 class="title">Nike Epic React Flyknit</h1>
+                <h3>A RUNNER'S CHOICE, <br>EXPERIENCE THE COMFORT</h3>
+                <div class="sizes d-flex justify-content-around">
+                    <button>9</button>
+                    <button class="active">10</button>
+                    <button>11</button>
+                    <button>12</button>
+                </div>
+                <div class="purchase">
+                    <button class="purchaseText">Add to cart<i class="fas fa-shopping-cart"></i></button>
+                </div>
+            </div>
+        </div>
+    </div>
             `
             }
 
