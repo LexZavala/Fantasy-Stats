19b372ddeb9bcd7cf878a5cc860ef4de4cfe66c6
Lex Zavala <lex.zavala7@gmail.com>
2021-07-07 09:50:33 -0500

    added css

--- js/script.js
+++ js/script.js
@@ -43,5 +43,47 @@ const getTeams = () => {
         }, 1000)
     });
 }
-
 getTeams();
+
+// These is animation Javascript code
+const card = document.querySelector('.cardContainer');
+const container = document.querySelector('.mainContainer');
+
+const title = document.querySelector('.title');
+const sneaker = document.querySelector('.sneaker img');
+const purchase = document.querySelector('.purchase');
+const description = document.querySelector('.info h3');
+const sizes = document.querySelector('.sizes');
+
+//moving animation container
+container.addEventListener("mousemove", function (e){
+    // console.log(e.pageY);
+    let xAxis = (window.innerWidth/2 - e.pageX) / 25;
+    let yAxis = (window.innerHeight/2 - e.pageY) / 25;
+    card.style.transform =  `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+
+});
+
+//Animate In
+container.addEventListener('mouseenter', function (e){
+    card.style.transition = 'none';
+    //POPOUT
+    title.style.transform = 'translateZ(90px)';
+    sneaker.style.transform = 'translateZ(90px)';
+    description.style.transform = 'translateZ(80px)';
+    purchase.style.transform = 'translateZ(70px)';
+    sizes.style.transform = 'translateZ(65px)';
+
+});
+
+//Animate Out
+container.addEventListener('mouseleave', function (e){
+    card.style.transition = 'all 0.5s ease';
+    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+    //POPBACK
+    title.style.transform ='translateZ(0px)';
+    sneaker.style.transform = 'translateZ(0px) rotateZ(0deg)';
+    description.style.transform = 'translateZ(0px)';
+    purchase.style.transform = 'translateZ(0px)';
+    sizes.style.transform = 'translateZ(0px)';
+});
