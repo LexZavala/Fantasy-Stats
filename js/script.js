9f1bb30b2bd413caeda18aef52dfe07de1020e20
Lex Zavala <lex.zavala7@gmail.com>
2021-07-07 09:29:26 -0500

    added css from other project

--- js/script.js
+++ js/script.js
@@ -19,29 +19,23 @@ const getTeams = () => {
         setTimeout(function () {
             let htmlStr = '';
             for (let team of teams) {
-                htmlStr += `<div><h3>${team.City}</h3></div>
-            <div><img class="teamLogo" src=${team.WikipediaLogoUrl}></div>
-            
-            
-            <div class="mainContainer d-flex justify-content-center align-items-center">
-                <div class="cardContainer">
-                    <div class="sneaker d-flex align-items-center justify-content-center">
-                        <div class="circle"></div>
-                        <img class="teamLogo" src=${team.WikipediaLogoUrl}>
-                    </div>
-                    <div class="info">
-                        <h1 class="title">${team.City}</h1>
-                        <h3>A RUNNER'S CHOICE, <br>EXPERIENCE THE COMFORT</h3>
-                        <div class="sizes d-flex justify-content-around">
-                            <p>This is some info about the team</p>
+                htmlStr += `
+                <div class="mainContainer d-flex justify-content-center align-items-center">
+                    <div class="cardContainer">
+                        <div class="sneaker d-flex align-items-center justify-content-center">
+                            <div class="circle"></div>
+                            <img class="teamLogo" src=${team.WikipediaLogoUrl}>
                         </div>
-                        <div class="purchase">
-                            <button class="purchaseText">Add to cart<i class="fas fa-shopping-cart"></i></button>
+                        <div class="info">
+                            <h1 class="title">${team.City}</h1>
+                            <h3>A RUNNER'S CHOICE, <br>EXPERIENCE THE COMFORT</h3>
+                            <div class="sizes d-flex justify-content-around">
+                                <p>This is some info about the team</p>
+                            </div>
                         </div>
-                    </div>
-                </div>  
-            </div>
-            `
+                    </div>  
+                </div>
+                `
             }
 
             $('#teamsContainer').html(htmlStr);
