a5c95b4f5d3b3729d25217b311526b69efe2fc16
Lex Zavala <lex.zavala7@gmail.com>
2021-07-09 23:38:06 -0500

    deleted some empty spaces

--- js/script.js
+++ js/script.js
@@ -61,7 +61,6 @@ container.addEventListener("mousemove", function (e){
     let xAxis = (window.innerWidth/2 - e.pageX) / 25;
     let yAxis = (window.innerHeight/2 - e.pageY) / 25;
     card.style.transform =  `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-
 });
 
 //Animate In
